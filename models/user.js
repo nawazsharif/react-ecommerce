740299259ffea7561be6ebe2bcd1dbd39201bd38
nawaz sharif <nawazsharif.csm@gmail.com>
2019-09-22 17:07:39 +0600

    push

--- models/user.js
+++ models/user.js
@@ -45,25 +45,18 @@ const userSchema = new mongoose.Schema(
 userSchema
   .virtual('password')
   .set(function (password) {
-    // create temporary variable called _password
     this._password = password
-    // generate a timestamp
     this.salt = uuidv1()
-    // encryptPassword()
     this.hashed_password = this.encryptPassword(password)
   })
   .get(function () {
     return this._password
   })
 
-// methods
 userSchema.methods = {
-  authenticate: function (plainText) {
-    return this.encryptPassword(plainText) === this.hashed_password
-  },
-
   encryptPassword: function (password) {
-    if (!password) return ''
+    if (!password) {
+    }
     try {
       return crypto
         .createHmac('sha1', this.salt)
@@ -74,11 +67,4 @@ userSchema.methods = {
     }
   }
 }
-
-// pre middleware
-userSchema.pre('remove', function (next) {
-  Post.remove({ postedBy: this._id }).exec()
-  next()
-})
-
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model('user', userSchema)
